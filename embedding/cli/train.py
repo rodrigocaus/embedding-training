aabe88a905a0f2957abe511984a7085069cce16b
rodrigocaus <rodrigo.caus@gobots.com.br>
2025-05-13 17:28:45 -0300

    feat: map training config to training code

--- embedding/cli/train.py
+++ embedding/cli/train.py
@@ -1,8 +1,138 @@
-from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer
-
+import os
 import config
+import preprocess
+from losses import kl
+from util import logger
+
+from torch import nn
+from typing import NamedTuple, Optional
+from datasets import load_dataset, Dataset
+from sentence_transformers import (
+    losses, evaluation,
+    SentenceTransformer, SentenceTransformerTrainer
+)
+
+
+class Objective(NamedTuple):
+    name: str
+    loss: nn.Module
+    train_dataset: Dataset
+    validation_dataset: Optional[Dataset]
+
+
+def map_objective(model: SentenceTransformer, spec: config.TrainingObjective) -> Objective:
+    objective_type = spec.type
+    if objective_type not in ("contrastive", "similarity", "distillation"):
+        raise ValueError(f"Unknown objective type: {objective_type}")
+
+    ## LOSS DEFINITION ##
+    if objective_type == "contrastive":
+        loss = losses.MultipleNegativesSymmetricRankingLoss(
+            model=model, **spec.loss_args
+        )
+        dataset_preprocessor = preprocess.EFAQRankingGenerator
+
+    elif objective_type == "similarity":
+        loss = losses.CoSENTLoss(model=model, **spec.loss_args)
+        dataset_preprocessor = preprocess.EFAQCosineSimilarityGenerator
+
+    elif objective_type == "distillation":
+        if not spec.teacher:
+            raise ValueError(
+                "Teacher model not specified for distillation loss")
+
+        teacher = SentenceTransformer(spec.teacher.name, **spec.teacher.args)
+        loss = kl.KLSimilarityLoss(
+            student=model, teacher=teacher, **spec.loss_args
+        )
+        dataset_preprocessor = preprocess.EFAQRankingGenerator
+
+    if spec.matryoshka:
+        loss = losses.MatryoshkaLoss(
+            model=model,
+            loss=loss,
+            **spec.matryoshka
+        )
+
+    ## DATASETS DEFINITION ##
+    train_dataset_spec = spec.datasets.train
+    validation_dataset_spec = spec.datasets.validation
+
+    train_raw_dataset = load_dataset(
+        train_dataset_spec.name, **train_dataset_spec.args
+    )
+    train_dataset_preprocess = dataset_preprocessor(
+        train_raw_dataset, **train_dataset_spec.preprocess_args
+    )
+    train_dataset = Dataset.from_generator(train_dataset_preprocess)
+
+    if validation_dataset_spec:
+        validation_raw_dataset = load_dataset(
+            validation_dataset_spec.name, **validation_dataset_spec.args
+        )
+        validation_dataset_preprocess = dataset_preprocessor(
+            validation_raw_dataset, **validation_dataset_spec.preprocess_args
+        )
+        validation_dataset = Dataset.from_generator(
+            validation_dataset_preprocess
+        )
+    else:
+        validation_dataset = None
+
+    return Objective(objective_type, loss, train_dataset, validation_dataset)
+
+
+def load_evaluator(spec: config.EvaluatorConfig) -> evaluation.InformationRetrievalEvaluator:
+    raw_dataset = load_dataset(spec.dataset.name, **spec.dataset.args)
+    dataset_preprocessor = preprocess.EFAQRetrievalTransform(
+        raw_dataset, **spec.dataset.preprocess_args
+    )
+    retrieval_data = dataset_preprocessor()
+
+    return evaluation.InformationRetrievalEvaluator(
+        **retrieval_data,
+        **spec.args
+    )
 
 
 def main(filename: str):
     training_config = config.load(filename)
-    print(training_config)
+
+    training_args = training_config.args
+    training_args.output_dir = os.path.join(
+        training_config.output_dir, training_config.run_name
+    )
+
+    ## Load Base Model ##
+    base_model = SentenceTransformer(
+        training_config.base_model.name, **training_config.base_model.args
+    )
+
+    ## Load Objectives ##
+    train_dataset = {}
+    validation_dataset = {}
+    losses = {}
+    for objective_spec in training_config.objectives:
+        objective = map_objective(base_model, objective_spec)
+        losses[objective.name] = objective.loss
+        train_dataset[objective.name] = objective.train_dataset
+        if objective.validation_dataset:
+            validation_dataset[objective.name] = objective.validation_dataset
+
+    if not validation_dataset:
+        validation_dataset = None
+
+    ## Load Evaluator ##
+    evaluator = load_evaluator(training_config.evaluator)
+
+    ## Trainer ##
+    trainer = SentenceTransformerTrainer(
+        model=base_model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=validation_dataset,
+        loss=losses,
+        evaluator=evaluator,
+    )
+
+    trainer.train()
--- embedding/config.py
+++ embedding/config.py
@@ -1,26 +1,62 @@
 import json
 import yaml
 import pathlib
-from dataclasses import dataclass, is_dataclass, field
-from typing import Any, Dict, List, Literal, Optional
+from dataclasses import dataclass, is_dataclass, field, fields as dataclass_fields
+from typing import Any, Dict, List, Literal, Optional, Union, get_origin, get_args
 from sentence_transformers import SentenceTransformerTrainingArguments
 
 
 Object = Dict[str, Any]
+ObjectiveType = Literal["contrastive", "similarity", "distillation"]
 
 
 @dataclass
 class ModelConfig:
+    """Configuration for a model.
+
+    Attributes:
+        name (str): The name or path of the model.
+        args (dict): Additional arguments for loading the model.
+            Defaults to an empty dictionary.
+    """
     name: str
     args: Object = field(default_factory=dict)
 
 
 @dataclass
+class DatasetConfig:
+    """
+    Configuration for a dataset.
+
+    Attributes:
+        name (str): The name or path of the dataset.
+        args (dict): Additional arguments for loading the dataset.
+        preprocess_args (dict): Arguments for preprocessing the dataset.
+    """
+    name: str
+    args: Object = field(default_factory=dict)
+    preprocess_args: Object = field(default_factory=dict)
+
+
+@dataclass
+class TrainingDatasets:
+    train: DatasetConfig
+    validation: Optional[DatasetConfig] = field(default=None)
+
+
+@dataclass
 class TrainingObjective:
-    type: Literal["contrastive", "similarity", "distillation"]
+    type: ObjectiveType
+    datasets: TrainingDatasets
     loss_args: Object = field(default_factory=dict)
     matryoshka: Object = field(default_factory=dict)
     teacher: Optional[ModelConfig] = field(default=None)
+
+
+@dataclass
+class EvaluatorConfig:
+    dataset: DatasetConfig
+    args: Object = field(default_factory=dict)
 
 
 @dataclass
@@ -30,7 +66,16 @@ class TrainingConfigV1:
     output_dir: str
     objectives: List[TrainingObjective]
     args: SentenceTransformerTrainingArguments
-    evaluator_args: Object = field(default_factory=dict)
+    evaluator: EvaluatorConfig
+
+
+def _is_optional_type(field) -> bool:
+    origin_type = get_origin(field)
+    args_type = get_args(field)
+
+    return (origin_type is Union and
+            len(args_type) == 2 and
+            type(None) in args_type)
 
 
 def to_dataclass(data: dict, dataclass_type: type):
@@ -39,21 +84,25 @@ def to_dataclass(data: dict, dataclass_type: type):
 
     field_types = {
         field.name: field.type
-        for field in dataclass_type.__dataclass_fields__.values()
+        for field in dataclass_fields(dataclass_type)
     }
     fields = {}
 
     for key, value in data.items():
         if key not in field_types:
-            raise ValueError(f"Key '{key}' not found in dataclass definition")
+            raise ValueError(
+                f"Key '{key}' not found in dataclass {dataclass_type} definition")
 
         field_type = field_types[key]
+        if _is_optional_type(field_type):
+            field_type = get_args(field_type)[0]
 
         if isinstance(value, dict) and is_dataclass(field_type):
             fields[key] = to_dataclass(value, field_type)
         elif isinstance(value, list) and field_type.__args__ and is_dataclass(field_type.__args__[0]):
+            field_type = get_args(field_type)[0]
             fields[key] = [
-                to_dataclass(item, field_type.__args__[0])
+                to_dataclass(item, field_type)
                 if isinstance(item, dict) else item for item in value
             ]
         else:
