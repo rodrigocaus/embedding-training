8f98b2bd53b3b308a066f7ef13669b30151f3124
Visot <victor.sot.c@gmail.com>
2025-05-16 17:56:58 -0300

    rerankers evaluators

--- embedding/evaluation/sts_evaluator.py
+++ embedding/evaluation/sts_evaluator.py
@@ -4,9 +4,13 @@ import pandas as pd
 from typing import List
 from datasets import load_dataset
 from sentence_transformers import SentenceTransformer
-from utils.preprocess import format_retrieval_evaluator
+
+
+from utils.preprocess import format_retrieval_gosim3
 
 from evaluators.information_retrieval import IR_evaluator
+from evaluators.sts import sts_evaluator
+
 
 def get_dims(hidden_size: int) -> List[int]:
     return [
@@ -14,17 +18,27 @@ def get_dims(hidden_size: int) -> List[int]:
         if s <= hidden_size
     ]
 
-
+def map_label_sts(example):
+    dict_to_value = {
+        "similar":2,
+        "almost similar": 1,
+        "dissimilar": 0
+    }
+    example['score'] = dict_to_value[example["label"]]
+    return example
+ 
 
 if __name__ == "__main__":
     ### --- Model loading ---###
-    #MODEL_PATH = "Alibaba-NLP/gte-multilingual-base"
+    ##MODEL_PATH = "Alibaba-NLP/gte-multilingual-base"
     MODEL_PATH = "embedding/evaluation/models/multilingual-e5-base-e-faq-c3"
     #MODEL_PATH = "GoBotsAI/multilingual-e5-large-finetuned-e-faq"
-    original = "models" not in MODEL_PATH
+    original = "GoBotsAI" not in MODEL_PATH and "models" not in MODEL_PATH
     print(MODEL_PATH)
-    dataset = load_dataset('GoBotsAI/e-faq')
+    dataset = load_dataset('GoBotsAI/GoSim-3')['test']
+    dataset = dataset.map(map_label_sts)
 
+    # GoSim-3 -> sentence1, sentence2, label [similar, dissimilar,almost similar]
     if "e5" in MODEL_PATH:
         model = SentenceTransformer(
             MODEL_PATH, prompts={"question": "query: "}, default_prompt_name="question"
@@ -37,12 +51,21 @@ if __name__ == "__main__":
         # just using full dimension
         dimension = [dimension[-1]]
     for dim in dimension:
+        # compute_STS
+        print("Computing STS ...")
+        sts_evaluator(
+            model_path=MODEL_PATH,
+            model=model,
+            test_dataset=dataset,
+            col1="sentence1",
+            col2="sentence2",
+            score="score",
+            dataset_name="GoSim3",
+            original=original,
+            dim=dim,
+        )
         # compute_IR
         print("Computing IR ...")
-        formatted_data = format_retrieval_evaluator(dataset=dataset['pt'])
-        
+        formatted_data = format_retrieval_gosim3(dataset=dataset)
         IR_evaluator(model=model,model_path=MODEL_PATH,lang='pt',formatted_data=formatted_data,dim=dim,original=original)
-        formatted_data = format_retrieval_evaluator(dataset=dataset['es'])
-        IR_evaluator(model=model,model_path=MODEL_PATH,lang='es',formatted_data=formatted_data,dim=dim,original=original)
-
         
